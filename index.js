5611a3eb50107f0c1b3056616c983cf350c1a657
[A[Balexia turner <alexiaturner411@gmail.com>
2022-08-09 18:52:34 -0700

    problem 8

--- index.js
+++ index.js
@@ -320,12 +320,13 @@ Use artistInfo to do the following:
 
 🌟 EXAMPLE: Invoking artistInfo(artists, 'Frida Kahlo') will return: 
   "Frida Kahlo de Rivera (Spanish pronunciation: [ˈfɾiða ˈkalo]; born Magdalena Carmen Frida Kahlo y Calderón; 6 July 1907 – 13 July 1954) was a Mexican artist who painted many portraits, self-portraits and works inspired by the nature and artifacts of Mexico. Inspired by the country's popular culture, she employed a naïve folk art style to explore questions of identity, postcolonialism, gender, class and race in Mexican society. Her paintings often had strong autobiographical elements and mixed realism with fantasy. In addition to belonging to the post-revolutionary Mexicayotl movement, which sought to define a Mexican identity, Kahlo has been described as a surrealist or magical realist.Born to a German father and a mestiza mother, Kahlo spent most of her childhood and adult life at her family home in Coyoacán, La Casa Azul, now known and publicly accessible as the Frida Kahlo Museum. She was disabled by polio as a child. Until a traffic accident at age eighteen caused lifelong pain and medical problems, she had been a promising student headed for medical school. During her recovery, she returned to her childhood hobby of art with the idea of becoming an artist."
-*/
+*/ 
 
 function artistInfo(array, name){
-//return  `${array[name].bio}` 
+let info = array.find(element => element.name === name );
+return info.bio;
 }
-console.log(artistInfo(artists, 'Frida Khalo'));
+console.log("test 8", artistInfo(artists, "Frida Kahlo"));
 
 
 
@@ -339,10 +340,9 @@ Use artistByCountry to do the following:
 🌟 EXAMPLE: Invoking artistByCountry(artists, 'Spanish') will return: [ 'Salvador Dali', 'Pablo Picasso', 'Francisco Goya']
 */
 
-function artistByCountry(array, nationality){
-//filter by country
+function artistByCountry(/*array, nationality*/){
 }
-console.log(artistByCountry(artists, 'spanish'));
+console.log(artistByCountry(/*artists, 'spanish'*/));
 
 
 
